1c0230872bcc247ec136b4e0e629a52b302ef20f
Song jae hyeok <wakij6587@naver.com>
2021-01-05 11:59:16 +0900

    카운트 이벤트, 우주비행사 움직임 추가
    카운트 이벤트, 마우스 움직임에 따른 우주비행사 포지션 변경 이벤트 추가

--- script/script.js
+++ script/script.js
@@ -1,1 +1,63 @@
 console.clear();
+
+
+const count = document.querySelectorAll("h2.number_count");
+let is_Count = false;
+
+function counter(target, start, end, speed){
+    if(is_Count){
+        return 
+    }
+    let i = start;
+    let timer = setInterval(()=>{
+        i++;
+        target.innerHTML = i;
+        if(i == end){
+            clearInterval(timer);
+        }
+    }, speed)
+}
+
+// Scroll Fade In, Header Bg Change 
+function scroll_event(){
+    const scroll_count = document.querySelectorAll(".scrollCount");
+    
+    window.addEventListener('scroll', scrollEvent);
+    function scrollEvent(){
+        // Count Event
+        let scroll_count_top = [];
+        for(let i = 0 ; i < scroll_count.length ; i++){
+            scroll_count_top[i] = scroll_count[i].getBoundingClientRect().top;
+            if(scroll_count_top[i] < window.innerHeight * 0.9){
+                counter(count[0], 6674, 6774, 20);
+                counter(count[1], 932, 1032, 20);
+                counter(count[2], 1550, 1650, 20);
+                is_Count = true;
+            }
+        }
+    }
+}
+
+function move_astronaut(){
+    document.addEventListener('mousemove', function(e){
+        const astronaut = document.querySelector("img.astronaut");
+        let mouse_x = getTransformValue(e.clientX,window.innerWidth,25);
+        let mouse_y = getTransformValue(e.clientY,window.innerHeight,25);
+        astronaut.style.left = mouse_x + "px"
+        astronaut.style.top = mouse_y + "px"
+        astronaut.style.filter = "drop-shadow(" + -mouse_x + "px " + mouse_y + "px 20px #000)"
+
+    });
+    function getTransformValue(v1,v2,value){
+        return (v1/v2*value-value/2).toFixed(1);                        
+    }
+}
+
+
+
+
+function init(){
+    scroll_event();
+    move_astronaut();
+}
+init();
