a3547262add779fbb6268326462086ffe1e153eb
Song jae hyeok <wakij6587@naver.com>
2021-01-06 23:18:00 +0900

    threejs 스크롤시 카메라 이동, 행성 위치 설정
    threejs 스크롤시 카메라 이동, 행성 위치 설정

--- script/script.js
+++ script/script.js
@@ -3,6 +3,7 @@ console.clear();
 
 // Scroll Count Up
 const count = document.querySelectorAll("h2.number_count");
+const count_txt = document.querySelectorAll("p.count_txt");
 let is_Count = false;
 function counter(target, start, end, speed){
     if(is_Count){
@@ -13,6 +14,9 @@ function counter(target, start, end, speed){
         i++;
         target.innerHTML = i;
         if(i == end){
+            for(let i = 0 ; i < count_txt.length ; i++){
+                count_txt[i].classList.add("active_count_txt");
+            }
             clearInterval(timer);
         }
     }, speed)
@@ -111,7 +115,7 @@ function planet(){
     document.getElementById("threejs_planet").appendChild(renderer.domElement);
     // control = new THREE.OrbitControls(camera, renderer.domElement);
 
-    //SIZE ---------
+    //SIZE
     renderer.setSize(window.innerWidth, window.innerHeight);
     window.addEventListener("resize", function(){
         var width = window.innerWidth;
@@ -122,9 +126,8 @@ function planet(){
     })
     renderer.setClearColor( 0x000000, 0);
 
-    //CAMERA--------
+    //CAMERA
     camera.position.set(0, 0, 3);
-    camera.lookAt(0, 0, 0); 
     
     // Light
     const DirecLight = new THREE.DirectionalLight( 0xffffff, 2);
@@ -162,19 +165,21 @@ function planet(){
         }
     );
 
-    // MILLER --------------
+
+    // MILLER 
     var miller_Geom= new THREE.SphereBufferGeometry(1, 9 ,14, 0, 6.3, 1, 6.3);
     var miller_Mat = new THREE.MeshPhongMaterial({
-        color: 0xa4dcdc,
-        //와이어프레임으로 보이게 하였다.
+        // color: 0xa4dcdc,
+        color: 0xffffff,
         wireframe: true
     });
     var miller = new THREE.Mesh(miller_Geom, miller_Mat);
+
     scene.add(miller);
     miller.scale.set(1, 1, 1);
-    miller.position.set(0, 0, 0);
+    miller.position.set(0, -4, 0);
     
-    //MANN------------------
+    //MANN
     var mann_Geom= new THREE.TorusKnotBufferGeometry(0.5, 1, 67, 20, 9, 20);
     var mann_Mat = new THREE.MeshPhongMaterial({
         color: 0x505050,
@@ -183,23 +188,95 @@ function planet(){
     var mann = new THREE.Mesh(mann_Geom, mann_Mat);
     scene.add(mann);
     mann.scale.set(0.7, 0.7, 0.7);
-    mann.position.set(0, 0, 0);
+    mann.position.set(0, -8, 0);
 
-    //COOPER------------
+
+    // Fifth
+    var earth_fifth = new THREE.Object3D;
+    var earth_fifth_loader = new THREE.OBJLoader();
+    earth_fifth_loader.load(
+        "./obj/earth.obj",
+        function(object){
+            earth_fifth = object;
+            earth_fifth.scale.set(1, 1, 1);
+            earth_fifth.position.set(0, -12, 0);
+            scene.add(earth_fifth);
+        }
+    );
+
+    var miller_fifth = new THREE.Mesh(miller_Geom, miller_Mat);
+    miller_fifth.position.set(0, -12, 0);
+    scene.add(miller_fifth);
+
+    var mann_fifth = new THREE.Mesh(mann_Geom, mann_Mat);
+    mann_fifth.scale.set(0.7, 0.7, 0.7);
+    mann_fifth.position.set(0, -12, 0);
+    scene.add(mann_fifth);
+
+    var cooper = new THREE.Object3D;
     var cooper_loader = new THREE.OBJLoader();
     cooper_loader.load(
         "./obj/cooper.obj",
         function(object){
             cooper = object;
             cooper.scale.set(2, 2, 2);
-            cooper.position.set(0, 0, 0);
+            cooper.position.set(0, -12, 0);
             scene.add(cooper);
         }
     );
     
-   
 
-    //RENDER-------------------------------------------------------------------------------
+    // Cooper
+    var cooper = new THREE.Object3D;
+    var cooper_loader = new THREE.OBJLoader();
+    cooper_loader.load(
+        "./obj/cooper.obj",
+        function(object){
+            cooper = object;
+            cooper.scale.set(2, 2, 2);
+            cooper.position.set(0, -16, 0);
+            scene.add(cooper);
+        }
+    );
+
+
+    // Interstellar
+    var main = new THREE.Object3D;
+    var main_loader = new THREE.OBJLoader();
+    main_loader.load(
+        "./obj/main2.obj",
+        function(object){
+            main = object;
+            main.scale.set(3, 3, 3);
+            main.position.set(0, -21.7, 0);
+            main.rotation.set(0, 30, 0);
+            scene.add(main);
+        }
+    );
+   
+    const visual_area = document.querySelector("section.visual_area");
+    const visual_area_top = visual_area.offsetTop;
+    let visual_area_slice = (visual_area.clientHeight - window.innerHeight) / 6;
+    let cameraPos = "earth";
+    window.addEventListener("scroll", change_planet);
+    function change_planet(){
+        let cur_scroll = document.documentElement.scrollTop;
+        if(cur_scroll > visual_area_top + (visual_area_slice * 5)){
+            cameraPos = "interstellar";
+        }else if(cur_scroll > visual_area_top + (visual_area_slice * 4)){
+            cameraPos = "cooper";
+        }else if(cur_scroll > visual_area_top + (visual_area_slice * 3) ){
+            cameraPos = "fifth";
+        }else if(cur_scroll > visual_area_top + (visual_area_slice * 2)){
+            cameraPos = "mann";
+        }else if(cur_scroll > visual_area_top + (visual_area_slice * 1)){
+            cameraPos = "miller";
+        }else if(cur_scroll > visual_area_top){
+            cameraPos = "earth";
+        }
+    }
+
+    //RENDER
     var renderScene = new function renderScene() {
         requestAnimationFrame(renderScene);
 
@@ -209,9 +286,48 @@ function planet(){
         mann.rotation.y += 0.0004;
         cooper.rotation.y += 0.0004;
 
+        // console.log(camera.position.y);
+        if(cameraPos == "earth"){
+            if(camera.position.y < 0){
+                camera.position.y += 0.03; 
+            }
+        }else if(cameraPos == "miller"){
+            if(camera.position.y < -4){
+                camera.position.y += 0.03;
+            }
+            if(camera.position.y > -4){
+                camera.position.y -= 0.03;
+            }
+        }else if(cameraPos == "mann"){
+            if(camera.position.y < -8){
+                camera.position.y += 0.03; 
+            }
+            if(camera.position.y > -8){
+                camera.position.y -= 0.03; 
+            }
+        }else if(cameraPos == "fifth"){
+            if(camera.position.y < -12){
+                camera.position.y += 0.03; 
+            }
+            if(camera.position.y > -12){
+                camera.position.y -= 0.03; 
+            }
+        }else if(cameraPos == "cooper"){
+            if(camera.position.y < -16){
+                camera.position.y += 0.03; 
+            }
+            if(camera.position.y > -16){
+                camera.position.y -= 0.03; 
+            }
+        }else if(cameraPos == "interstellar"){
+            if(camera.position.y > -20){
+                camera.position.y -= 0.03; 
+            }
+        }
+
+
         // particle
         particle.rotation.y += 0.000006;
-
         renderer.render(scene,camera);
     }    
 }
